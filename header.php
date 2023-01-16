61272be4e27034f61e275002718dddeed153d23f
Sergio0227 <sergioaires0227@gmail.com>
2023-01-16 01:59:10 +0100

    Update

---
+++ header.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet" >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Login Page</title>
+</head>
+<header>
+    <nav>
+        <ul class="list-unstyled">
+            <li class="list-item"><a href="index.php">Create</a></li>
+            <li class="list-item"><a href="login_update.php">Update</a></li>
+            <li class="list-item"><a href="login_delete.php">Delete</a></li>
+            <li class="list-item"><a href="login_read.php">Read</a></li>
+        </ul>
+    </nav>
+</header>
+
+<?php
--- login_read.php
+++ login_read.php
@@ -1,16 +1,10 @@
 <?php   include "db.php";
         include "funtions.php";
+    include "header.php";
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <link href="style.css" rel="stylesheet" >
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Login Page</title>
-</head>
 
 <body>
 <div class="container">
